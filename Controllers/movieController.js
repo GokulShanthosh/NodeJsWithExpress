73726af0c888edec90bd320fcfabc0e04d6ea365
GokulShanthosh <gokulshanthoshm@gmail.com>
2025-04-18 10:51:47 +0530

    Working_CRUD_2

--- Controllers/movieController.js
+++ Controllers/movieController.js
@@ -15,53 +15,59 @@ exports.getHighestRated = (req, res, next) => {
 
 exports.getAllMovies = async (req, res) => {
   try {
+    const features = new ApiFeatures(Movie.find(), req.query)
+      .filter()
+      .sort()
+      .limitFields()
+      .pagination();
+
     // 1. Create a shallow copy of req.query to avoid modifying the original
-    const queryObj = { ...req.query };
+    // const queryObj = { ...req.query };
 
-    // 2. Exclude special field names from filtering
-    const excludedFields = ["page", "sort", "limit", "fields"];
-    excludedFields.forEach((field) => delete queryObj[field]);
+    // // 2. Exclude special field names from filtering
+    // const excludedFields = ["page", "sort", "limit", "fields"];
+    // excludedFields.forEach((field) => delete queryObj[field]);
 
-    // 3. Advanced filtering for gte, gt, lte, lt
-    let queryStr = JSON.stringify(queryObj);
-    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+    // // 3. Advanced filtering for gte, gt, lte, lt
+    // let queryStr = JSON.stringify(queryObj);
+    // queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
 
-    // Start building the query
-    let query = Movie.find(qs.parse(JSON.parse(queryStr)));
+    // // Start building the query
+    // let query = Movie.find(qs.parse(JSON.parse(queryStr)));
 
-    // 4. Sorting
-    if (req.query.sort) {
-      const sortBy = req.query.sort.split(",").join(" ");
-      query = query.sort(sortBy);
-    } else {
-      query = query.sort("-createdAt"); // Default sorting
-    }
+    // // 4. Sorting
+    // if (req.query.sort) {
+    //   const sortBy = req.query.sort.split(",").join(" ");
+    //   query = query.sort(sortBy);
+    // } else {
+    //   query = query.sort("-createdAt"); // Default sorting
+    // }
 
-    // 5. Field limiting
-    if (req.query.fields) {
-      const fields = req.query.fields.split(",").join(" ");
-      query = query.select(fields);
-    } else {
-      query = query.select("-__v"); // Exclude __v field by default
-    }
+    // // 5. Field limiting
+    // if (req.query.fields) {
+    //   const fields = req.query.fields.split(",").join(" ");
+    //   query = query.select(fields);
+    // } else {
+    //   query = query.select("-__v"); // Exclude __v field by default
+    // }
 
-    // 6. Pagination
-    const page = parseInt(req.query.page) || 1;
-    const limit = parseInt(req.query.limit) || 10;
-    const skip = (page - 1) * limit;
+    // // 6. Pagination
+    // const page = parseInt(req.query.page) || 1;
+    // const limit = parseInt(req.query.limit) || 10;
+    // const skip = (page - 1) * limit;
 
-    query = query.skip(skip).limit(limit);
+    // query = query.skip(skip).limit(limit);
 
-    // Check if page exists
-    if (req.query.page) {
-      const moviesCount = await Movie.countDocuments();
-      if (skip >= moviesCount) {
-        throw new Error("This page does not exist");
-      }
-    }
+    // // Check if page exists
+    // if (req.query.page) {
+    //   const moviesCount = await Movie.countDocuments();
+    //   if (skip >= moviesCount) {
+    //     throw new Error("This page does not exist");
+    //   }
+    // }
 
     // Execute query
-    const movies = await query;
+    const movies = await features.query;
 
     // Send response
     res.status(200).json({
--- Utils/ApiFeatures.js
+++ Utils/ApiFeatures.js
@@ -1,5 +1,65 @@
+const qs = require("qs");
 class ApiFeatures {
-  constructor(query, queryStr) {}
+  constructor(query, queryStr) {
+    (this.query = query), (this.queryStr = queryStr);
+  }
+
+  filter() {
+    // 1. Create a shallow copy of req.query to avoid modifying the original
+    const queryObj = { ...this.queryStr };
+
+    // 2. Exclude special field names from filtering
+    const excludedFields = ["page", "sort", "limit", "fields"];
+    excludedFields.forEach((field) => delete queryObj[field]);
+
+    // 3. Advanced filtering for gte, gt, lte, lt
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+
+    // Start building the query
+    let query = this.query.find(qs.parse(JSON.parse(queryStr)));
+
+    return this;
+  }
+
+  sort() {
+    // 4. Sorting
+    if (this.queryStr.sort) {
+      const sortBy = this.queryStr.sort.split(",").join(" ");
+      this.query = this.query.sort(sortBy);
+    } else {
+      query = query.sort("-createdAt"); // Default sorting
+    }
+    return this;
+  }
+
+  limitFields() {
+    if (this.queryStr.fields) {
+      const fields = this.queryStr.fields.split(",").join(" ");
+      this.query = this.query.select(fields);
+    } else {
+      this.query = this.query.select("-__v"); // Exclude __v field by default
+    }
+    return this;
+  }
+
+  pagination() {
+    // 6. Pagination
+    const page = parseInt(this.queryStr.page) || 1;
+    const limit = parseInt(this.queryStr.limit) || 10;
+    const skip = (page - 1) * limit;
+
+    this.query = this.query.skip(skip).limit(limit);
+
+    // Check if page exists
+    // if (req.query.page) {
+    //   const moviesCount = await Movie.countDocuments();
+    //   if (skip >= moviesCount) {
+    //     throw new Error("This page does not exist");
+    //   }
+    // }
+    return this;
+  }
 }
 
-export default ApiFeatures;
+module.exports = ApiFeatures;
